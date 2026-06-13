1e5a21727b423a2230f0669efd5a6714e7f46223
minus-1 <sgone2842@GMAIL.COM>
2026-06-13 08:20:52 +0530

    feat: added multi doc support
    - Added Document model
    - Refactored App.load() and InvertedIndex.java to support loading and indexing multiple documents

--- src/main/java/com/mns/wordfinder/app/App.java
+++ src/main/java/com/mns/wordfinder/app/App.java
@@ -1,12 +1,20 @@
 package com.mns.wordfinder.app;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
 
 import com.mns.wordfinder.index.InvertedIndex;
 import com.mns.wordfinder.load.Load;
+import com.mns.wordfinder.model.Document;
 import com.mns.wordfinder.parser.Tokenizer;
 
 @Component
@@ -21,18 +29,39 @@ public class App {
     public void load() {
 
         Load loader = new Load();
-        String text = loader.file();
+        List<Document> docs = new ArrayList<>();;
 
-		Tokenizer tokenize = new Tokenizer();
-		List<String> tokens = tokenize.tokenize(text);
-		System.out.println(tokens);
+        // store stream of all files into document model
+        try (Stream<Path> stream = Files.list(Paths.get("doc_files"))) {
+
+            List<Path> filesList = stream.collect(Collectors.toList());
+
+            for (Path file : filesList) {
+                System.out.println("Fetching " + file + "...");
+                String content = loader.file(file);
+                Document doc = new Document(file.getFileName().toString(), content);
+                docs.add(doc);
+            }
 
 
-		for(String token: tokens){
-			indexer.addWord(token);
-		}
-        
-        indexer.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // tokenize and index all documents
+        Tokenizer tokenize = new Tokenizer();
+
+        for(Document doc : docs){
+
+            List<String> tokens = tokenize.tokenize(doc.getContent());
+
+            for(String token: tokens){
+                indexer.addWord(token, doc.getFilename());
+            }
+
+        }
+
+       // indexer.show();
 		indexer.stats();
 
     }
@@ -49,7 +78,7 @@ public class App {
 
             if(indexer.contains(word)){
                 System.out.println(word + " exists in doc!");
-                System.out.println("Freq -> " + indexer.frequency(word));
+                System.out.println("Data -> " + indexer.wordData(word));
             }
             else{
                 System.out.println(word + " does not exist in doc!");
